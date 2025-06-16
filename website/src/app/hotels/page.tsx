afa433bed4ddb095e371f5dd8d4af5a675f9fe05
Grant Bender <grant.bender13@gmail.com>
2025-06-16 19:41:41 -0400

    got grid pictures, still need to work on the landing page image...

--- website/src/app/hotels/page.tsx
+++ website/src/app/hotels/page.tsx
@@ -5,7 +5,24 @@ export default function Page() {
             <div className='min-h-screen flex items-center justify-center '>
                 <div className="bg-navy-blue p-8 rounded-lg max-w-md w-full"> 
                 <h1 className="text-2xl font-bold mb-6 text-center ">Hotels in the Area</h1>
-                    <h3>Coming soon</h3>
+                <ul>
+                    <li className="m-2">
+                    <a href="" target="_blank">
+                    <button 
+                        type="button"
+                        className="hover:bg-white bg-lighter-pink w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-navy-blue">
+                        Hotel 1
+                    </button></a>
+                    </li>
+                    <li className="m-2">
+                    <a href="" target="_blank">
+                    <button 
+                        type="button"
+                        className="hover:bg-white bg-lighter-pink w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-navy-blue">
+                        hotel 2
+                    </button></a>
+                    </li>
+                </ul>
             </div>
             </div>
         </main>
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import landingPic from "../../public/pics/Emma & Grant Engagement-07.jpg"
+import pic1 from "../../public/pics/Emma & Grant Engagement-05.jpg"
+import pic2 from "../../public/pics/Emma & Grant Engagement-10.jpg"
+import pic3 from "../../public/pics/Emma & Grant Engagement-11.jpg"
+import pic4 from "../../public/pics/Emma & Grant Engagement-27.jpg"
+import pic5 from "../../public/pics/Emma & Grant Engagement-30.jpg"
+import pic6 from "../../public/pics/Emma & Grant Engagement-33.jpg"
 import styles from "./home.module.css"
 
 export default function Home() {
   return (
     <main>
-        <div  className={styles.bgWrap}>
+ <div  className={styles.bgWrap}>
           <Image src={landingPic}
           alt="Emma and Grant"
           quality={100}
@@ -23,12 +29,58 @@ export default function Home() {
           Emma & Grant
         </h1>
         </div>
-        <div>
+        <div className="m-6">
           <h3 className={styles.bgTitle}>
           Our Wedding
         </h3>
         <p className={styles.bgText}>4:30pm Saturday, October 25, 2025<br/>2684 Columbus St, Grove City, OH 43123</p>
-            {/* pictures in a grid style */}
+        </div>
+        {/* pictures in a grid style */}
+        <div className="container mx-auto p-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6" >
+          <div className="">
+            <Image src={pic1}
+              alt="Emma and Grant 1"
+              quality={100}
+              placeholder="blur" 
+              className="rounded-2xl shadow-2xl"/>
+          </div>
+          <div>
+            <Image src={pic2}
+            alt="Emma and Grant 2"
+            quality={100}
+            placeholder="blur"
+            className="rounded-2xl shadow-2xl"/>
+            </div>
+            <div>
+            <Image src={pic3}
+            alt="Emma and Grant 3"
+            quality={100}
+            placeholder="blur"
+            className="rounded-2xl shadow-2xl"/>
+            </div>
+            <div>
+            <Image src={pic4}
+            alt="Emma and Grant 4"
+            quality={100}
+            placeholder="blur"
+            className="rounded-2xl shadow-2xl"/>
+            </div>
+            <div>
+            <Image src={pic5}
+            alt="Emma and Grant 5"
+            quality={100}
+            placeholder="blur"
+            className="rounded-2xl shadow-2xl"/>
+            </div>
+            <div>
+            <Image src={pic6}
+            alt="Emma and Grant 6"
+            quality={100}
+            placeholder="blur"
+            className="rounded-2xl shadow-2xl"/>
+            </div>
+            </div>
         </div>
     </main>
   );
